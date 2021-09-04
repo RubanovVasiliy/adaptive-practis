5005773f0583ca8f74542943e684951aca51ba9b
Vasiliy <rubanovas2005@gmail.com>
2021-09-05 01:31:09 +0700

    Header & 1st title done

---
+++ #source/js/main.js
@@ -0,0 +1,16 @@
+$('.wrapper').toggleClass('loaded');
+
+$('.icon-menu').click(function(event){
+    $(this).toggleClass('active');
+    $('.menu__body').toggleClass('active');
+    $('body').toggleClass('lock');
+});
+
+function ibg(){
+    $.each($('.ibg'), function(index, val) {
+        if($(this).find('img').length>0){
+            $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+        }
+    });
+}    
+ibg();
---
+++ js/script.js
@@ -0,0 +1,1 @@
+function ibg(){$.each($(".ibg"),(function(i,g){$(this).find("img").length>0&&$(this).css("background-image",'url("'+$(this).find("img").attr("src")+'")')}))}$(".wrapper").toggleClass("loaded"),$(".icon-menu").click((function(i){$(this).toggleClass("active"),$(".menu__body").toggleClass("active"),$("body").toggleClass("lock")})),ibg();
